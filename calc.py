0f02dfac0ca7a4c832833412bdb3867de5800490
Arpita Tiwari <arpitatevari@gmail.com>
2023-02-14 16:44:13 +0530

    while loop

---
+++ calc.py
@@ -0,0 +1,12 @@
+import os
+while True:
+    print('select a number')
+    print('1.Addition')
+    print('2.Subraction')
+    print('3.Multiplication')
+    print('4.Exit')
+    ch=input('Enter your choice:')
+    if ch=='1':
+        a=int(input('Enter first number:'))
+        a=int(input('Enter second number:'))
+        print
