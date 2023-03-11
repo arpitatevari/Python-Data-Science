627cba176fe468823b660710096c76384d1067cf
Arpita Tiwari <arpitatevari@gmail.com>
2023-03-11 12:17:50 +0530

    h

---
+++ dict.py
@@ -0,0 +1,15 @@
+show={
+    'title':'kaleidoscope',
+    'release date':'1 Jan-2023',
+    'season':'1',
+    'cast':'Giancarlo, Rosaline,Tati, Paz, Niousha',
+    'ott':'Netflix',
+    'rating':'8.9'
+}
+for key in show:
+    print(key)
+
+for k,v in show.items():
+    print(k,v)
+for v in show.values():
+    print(v)
